8dd8910dead4ff615752c9d3cafc198677e98bcd
vitorgmorigi <vitorgarcezmorigi@gmail.com>
2015-06-09 17:32:54 -0300

    versao 3
    tiros funcionando

--- main.cpp
+++ main.cpp
@@ -9,13 +9,18 @@ int main(void)
 	int height = 480; //resolução x
 
 	bool done = false; //variavel booleana para identificar se o programa terminou de ser executado
-	int pos_x = width / 2;
+	bool redraw = true;
+
+	int pos_x = 0;
 	int pos_y = height / 2;
+
+	int FPS = 60;
 
 	bool keys[4] ={false,false,false,false};
 
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_TIMER *timer = NULL;
 
 	if(!al_init())										//initialize Allegro
 		return -1;
@@ -29,16 +34,25 @@ int main(void)
 	al_install_keyboard(); //introduz o comando de teclas
 
 	event_queue = al_create_event_queue();
+	timer = al_create_timer(1.0 / FPS);
 
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 	al_register_event_source(event_queue, al_get_display_event_source(display));
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 
+    al_start_timer(timer);
 	while(!done)
 	{
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
 
-		if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
+		if(ev.type == ALLEGRO_EVENT_TIMER)
+            {
+                pos_x += 1;
+                redraw=true;
+            }
+
+		else if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
 		{
 			switch(ev.keyboard.keycode)
 			{
@@ -82,18 +96,23 @@ int main(void)
             done = true;
         }
 
-        pos_y -= keys[UP] * 10;
-        pos_y += keys[DOWN] * 10;
-        pos_x += keys[RIGHT] * 10;
-        pos_x -= keys[LEFT] * 10;
 
 
+
+
+
+        if(redraw && al_is_event_queue_empty(event_queue)){
+
+        redraw=false;
 
         al_draw_filled_rectangle(pos_x, pos_y-170, pos_x+30, pos_y-200, al_map_rgb(255,255,0));
+        al_draw_filled_rectangle(pos_x, pos_y-70, pos_x+30, pos_y-100, al_map_rgb(160,255,0));
 		al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, al_map_rgb(0,255,0));
-		al_draw_filled_rectangle(pos_x, pos_y+170, pos_x+30, pos_y+200, al_map_rgb(0,255,255));
+		al_draw_filled_rectangle(pos_x, pos_y+95, pos_x+30, pos_y+125, al_map_rgb(160,255,120));
+		al_draw_filled_rectangle(pos_x, pos_y+185, pos_x+30, pos_y+215, al_map_rgb(0,255,255));
 		al_flip_display();
 		al_clear_to_color(al_map_rgb(255,255,255));
+        }
 	}
 
 	al_destroy_event_queue(event_queue);
