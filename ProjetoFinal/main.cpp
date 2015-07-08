bb510a26df2375486db6fb743da172a4e6e5d045
Fernando <fspacheco@gmail.com>
2015-07-08 11:06:01 -0300

    Informação para compilar no Linux e pequenos ajustes.

--- ProjetoFinal/main.cpp
+++ ProjetoFinal/main.cpp
@@ -1,8 +1,8 @@
-#include <allegro5\allegro.h>
+#include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
-#include <allegro5\allegro_primitives.h>
-#include <allegro5\allegro_font.h>
-#include <allegro5\allegro_ttf.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 #include <stdio.h>
 #include "objects.h"				//Our primitive header file
 
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Electronics-vs-Zombies
 Projeto da disciplina de Programação em C feitos por Thales Lohn e Vítor Morigi
+
+Para compilar no Linux
+```
+g++ -Wall main.cpp -o electronics_vs_zombies -lallegro -lallegro_primitives -lallegro_font -lallegro_ttf -lallegro_image
+```
+
